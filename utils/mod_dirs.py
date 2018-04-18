d67af63451b845dcf51fa70ee6dd6f14a0d57db4
Mithun <mithun2595@gmail.com>
2018-04-18 15:36:47 -0700

    change count

--- utils/mod_dirs.py
+++ utils/mod_dirs.py
@@ -47,8 +47,10 @@ def main():
         label_path = os.path.join(dataset_dir, label)
         os.chdir(label_path)
         dirs_list = [dir_name for dir_name in os.listdir(label_path) if dir_name.startswith('-') and os.path.isdir(os.path.join(label_path, dir_name))]
-        Parallel(n_jobs=-1, verbose=True)(delayed(mod_dirs)(dir, label_path) for dir in dirs_list)
-
+        # Parallel(n_jobs=-1, verbose=True)(delayed(mod_dirs)(dir, label_path) for dir in dirs_list)
+        change_count += len(dirs_list)
+        print("CHANGE COUNT "+change_count)
+        
 
 if __name__ == '__main__':
     main()
