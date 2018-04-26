220271195c18386233c2aa66ea771829d7ecac27
Mithun <mithun2595@gmail.com>
2018-04-26 15:10:20 -0700

    memnet mods

--- models/pipeline.py
+++ models/pipeline.py
@@ -78,7 +78,7 @@ class Pipeline(object):
 
     def resize_crop(self, img: np.ndarray) -> np.ndarray:
         '''
-        resize the image frame to a random 224 by 224
+        resize the image frame to a random 299 by 299
         '''
 
         aspect_ratio = float(img.shape[1]) / float(img.shape[0])
@@ -91,12 +91,12 @@ class Pipeline(object):
 
         random.seed(datetime.now())
         resize = misc.imresize(img, (new_h, new_w), 'bilinear')
-        wrange = resize.shape[1] - 224
-        hrange = resize.shape[0] - 224
+        wrange = resize.shape[1] - 299
+        hrange = resize.shape[0] - 299
         w_crop = random.randint(0, wrange)
         h_crop = random.randint(0, hrange)
 
-        return resize[h_crop:h_crop + 224, w_crop:w_crop + 224]
+        return resize[h_crop:h_crop + 299, w_crop:w_crop + 299]
 
     def _parse(self, filename):
         frames, label = self.get_frames(filename)
@@ -202,4 +202,3 @@ if __name__ == '__main__':
                     print(f_data.shape, l_data)
                 except tf.errors.OutOfRangeError:
                     break
-
