bd9241cf9b3beb1f0b15125def393c8a65f67547
Dewal Gupta <dewalgupta@gmail.com>
2018-05-07 22:52:03 -0700

    moved flownet, and added bilinear interpolation

--- models/clocknet/clocknet.py
+++ models/clocknet/clocknet.py
@@ -13,6 +13,8 @@ from resnet import inception_resnet_v2
 
 # debug flag for debugging outputs
 _DEBUG = False
+H_f = W_f = 17
+D_f = 1088
 
 class Resnet:
     # TODO: plug in resnet
@@ -25,12 +27,12 @@ class Resnet:
     def call(self, inputs):
         # inputs is a 299x299x3 tensor
         # should return a 17x17x1088 tensor
-        return model.predict(np.array([inputs]))[0]
+        return self.model.predict(np.array([inputs]))[0]
 
     def callBatch(self, inputs):
         # inputs is a dx299x299x3 tensor
         # should return a dx17x17x1088 tensor
-        return model.predict(inputs)
+        return self.model.predict(inputs)
 
 class Flownet:
     # TODO: plug in Flownet
@@ -65,7 +67,6 @@ class ClockNet(snt.AbstractModule):
         self.prev_frame = tf.random_normal([self.mem_h, self.mem_w, self.df],
                                        mean=0, stddev=1)
 
-
     def compute_mem(self, curr_frame):
         # compute flow bewteen two frames
         flow = self.flownet.call(self.prev_frame, curr_frame)
@@ -76,8 +77,12 @@ class ClockNet(snt.AbstractModule):
         # save the prev frame to compute flow for next iteration
         self.prev_frame = self.curr_frame
 
-
-    def iterate(self, frame, is_training):
+    def iterate(self, frame):
+        """
+        Iterate does required computations on a frame level
+        :param frame: the frame (or time step) currently being processed 
+        :return: void - updates the memory 
+        """
         # computation per frame
         features = self.resnet.call(frame)
 
@@ -88,15 +93,53 @@ class ClockNet(snt.AbstractModule):
         # compute the new memory
         # corresponds to the 'A' function
         self.aggregate(features)
-        
-    
-    def bl_sample(self, before, after):
-        # before, after are the two features that we will
-        # bi-linearly sample. 
 
+    @staticmethod
+    def bl_kernel(a, b):
+        """
+        This function is the bilinear kernel. Calculates
+        max(0, 1 - |a-b|) as described in https://arxiv.org/pdf/1611.07715.pdf
+        """
+
+        return tf.maximum(0, 1 - tf.abs(a - b))
+
+    def bl_sample(self, features, displ):
+        """
+        
+        :param features: feature maps (aka the memory) that will be updated
+                        Expected to be a H_f x W_f x D_f tensor
+        :param displ: the displacement field
+                        Expected to be a H_f x W_f x 2 tensor
+        :return: returns the new feature map determine by a 
+                        bilinear interpolation as descrbied in:
+                        https://arxiv.org/pdf/1611.07715.pdf     
+        """
+
+        memory = tf.zeros([W_f, H_f, D_f])
+        for c in range(D_f):
+            # do BL-interpolation per channel
+            for px in range(W_f):
+                for py in range(H_f):
+                    d_px = displ[px, py, 0]
+                    d_py = displ[px, py, 1]
+                    results = 0
+                    for qx in range(W_f):
+                        for qy in range(H_f):
+                            prev = features[qx, qy, c]
+                            if prev == 0:
+                                continue
+
+                            gx = self.bl_kernel(qx, px+d_px)
+                            gy = self.bl_kernel(qy, py+d_py)
+                            results += prev * gx * gy
+
+                    memory[px, py, c] = results
+
+        return memory
 
     def build(self, inputs, is_training=False):
-        """Connects the model to inputs.
+        """
+        Connects the model to inputs.
 
         Args:
           inputs: Inputs to the model, which should have dimensions
@@ -112,6 +155,6 @@ class ClockNet(snt.AbstractModule):
                indexed by endpoint name.
         """
         for frame in inputs.get_shape()[1]:
-            self.iterate(frame, is_training)
+            self.iterate(frame)
 
 
