204e6177cb97cb8048dd5bf4448b70b286c97fa5
Mithun <mithun2595@gmail.com>
2018-05-15 01:09:06 -0700

    flow fix

--- models/tf-test.py
+++ models/tf-test.py
@@ -40,6 +40,7 @@ with tf.Session() as sess:
     print(np.array(mem).shape)
     flows = np.array(mem)[0]
     for i in range(flows.shape[0]):
+        print(flows[i])
         flow_img = lib.flow_to_image(flows[i])
         unique_name = 'flow_'+str(i)
         full_out_path = os.path.join('./imgs', unique_name + '.png')
