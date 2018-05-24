3da2cc2f41a7bfa6c3ef2caf8267694adfeb651c
Mithun <mithun2595@gmail.com>
2018-05-23 21:04:53 -0700

    modified comet

--- models/clocknet/clock_step.py
+++ models/clocknet/clock_step.py
@@ -62,6 +62,7 @@ class ClockStep:
         rep = tf.transpose(
             tf.expand_dims(tf.ones(shape=tf.stack([n_repeats, ])), 1), [1, 0])
         # rep = tf.cast(rep, 'int32')
+        x = tf.cast(x, 'float32')
         x = tf.matmul(tf.reshape(x, (-1, 1)), rep)
         return tf.reshape(x, [-1])
 
