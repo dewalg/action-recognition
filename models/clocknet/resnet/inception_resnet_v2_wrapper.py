de3f14343a148c76637b17d0be88f5901c2b712e
Mithun <mithun2595@gmail.com>
2018-05-24 18:33:04 -0700

    added changes

--- models/clocknet/resnet/inception_resnet_v2_wrapper.py
+++ models/clocknet/resnet/inception_resnet_v2_wrapper.py
@@ -23,7 +23,7 @@ class InceptionResNetV2():
 
                 self.outputs = {l.name: l.output for l in self.irv2.layers}
 
-            self.irv2_weights = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='IRV2/model')
+            self.irv2_weights = tf.get_collection(tf.GraphKeys.MODEL_VARIABLES, scope='IRV2/model')
 
             with tempfile.NamedTemporaryFile() as f:
                 self.tf_checkpoint_path = tf.train.Saver(self.irv2_weights).save(sess, f.name)
